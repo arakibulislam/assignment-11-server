8030c2ef14fd727781f3f5dbc631ab8617b5f615
maisha haider <arakibulislam>
2021-10-28 21:35:43 +0600

    aaa

--- index.js
+++ index.js
@@ -99,7 +99,11 @@ async function run() {
 run().catch(console.dir);
 
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
+app.get
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
